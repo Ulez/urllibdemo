da6081466698ad3c6097a1003f567e8b1d192a78
Ulez <1104128773@qq.com>
2018-03-25 15:20:38 +0800

    添加debug等级，输出发送的信息，header

--- src/MyHttpHandler.py
+++ src/MyHttpHandler.py
@@ -1,6 +1,7 @@
+#coding:utf-8
 import urllib2
 
-handler = urllib2.HTTPHandler()
+handler = urllib2.HTTPHandler(debuglevel=1)#添加debug，输出发送的信息，header
 opener = urllib2.build_opener(handler)
 request = urllib2.Request("http://www.baidu.com/")
 print opener.open(request).read()
